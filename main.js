0bebca7f5b04a0895b074ce1dbf674591d392872
Mathilde H <76396548+MathildeHmn@users.noreply.github.com>
2021-01-19 13:03:01 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
+// Constants names
 const maleFirstname = ["Gabriel", "Raphaël", "Léo", "Louis", "Lucas", "Adam", "Arthur", "Jules", "Hugo", "Maël", "Ethan", "Liam", "Paul", "Nathan", "Gabin", "Sacha", "Noah", "Tom", "Mohamed", "Théo", "Aaron", "Eden", "Noé", "Timéo", "Victor", "Martin", "Mathis", "Nolan", "Enzo", "Axel", "Léon", "Antoine", "Marius", "Naël", "Robin", "Valentin", "Clément", "Baptiste", "Tiago", "Rayan", "Samuel", "Amir", "Augustin", "Maxime", "Eliott", "Maxence", "Mathéo", "Gaspard", "Evan", "Alexandre"];
-
 const femaleFirstname = ["Emma", "Jade", "Louise", "Alice", "Chloé", "Inès", "Lina", "Léa", "Rose", "Léna", "Anna", "Mila", "Mia", "Ambre", "Elena", "Julia", "Manon", "Juliette", "Lou", "Zoé", "Camille", "Lola", "Eva", "Agathe", "Jeanne", "Lucie", "Nina", "Sarah", "Romane", "Inaya", "Charlotte", "Léonie", "Adèle", "Romy", "Iris", "Luna", "Louna", "Sofia", "Margaux", "Léana", "Clémence", "Olivia", "Elise", "Victoria", "Clara", "Victoire", "Aya", "Margot", "Noûr", "Giulia"];
 
 const getRandomNumber = (max) => Math.floor(Math.random() * max);
